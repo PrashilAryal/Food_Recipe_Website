8f9090f6213657f5b4e37175610278cf8879eb4e
karkikusal <karkikusal16@gmail.com>
2023-02-16 09:41:58 +0545

    test

--- Final_Project/resources/views/Recipe.blade.php
+++ Final_Project/resources/views/Recipe.blade.php
@@ -28,19 +28,30 @@
     <div class="recipe_area plus_padding">
         <div class="container">
             <div class="row">
-                <div class="col-xl-4 col-lg-4 col-md-6">
-                    <div class ="Single_recipe text-center">
+            <div class="col-xl-4 col-lg-4 col-md-6">
+            <div class="single_recipe text-center mb-20">
                         <div class="recipe_thumb">
-                            <img src="img/recipe/rec1.jpg" alt="">
+                            <img src="img/recipe/sand.jpg" alt="">
                         </div>
-                        <h3>Chowmein</h3>
+                        <h3>sandwich</h3>
                         
                         <p>Time Needs: 30 Mins</p>
                         <button type="button" class="btn btn-outline-secondary">View Full Recipe</button>
                     </div>
                 </div>
                 <div class="col-xl-4 col-lg-4 col-md-6">
-                    <div class="single_recipe text-center">
+                <div class="single_recipe text-center mb-20">
+                        <div class="recipe_thumb">
+                            <img src="img/recipe/pork.jpg" alt="">
+                        </div>
+                        <h3>Pork</h3>
+                        
+                        <p>Time Needs: 30 Mins</p>
+                        <button type="button" class="btn btn-outline-secondary">View Full Recipe</button>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-6 col-lg-4">
+                <div class="single_recipe text-center mb-20">
                         <div class="recipe_thumb">
                             <img src="img/recipe/rec3.jpg" alt="">
                         </div>
@@ -51,7 +62,7 @@
                     </div>
                 </div>
                 <div class="col-xl-4 col-lg-4 col-md-6">
-                    <div class="single_recipe text-center">
+                <div class="single_recipe text-center mb-20">
                         <div class="recipe_thumb">
                             <img src="img/recipe/rec1.jpg" alt="">
                         </div>
@@ -62,7 +73,7 @@
                     </div>
                 </div>
                 <div class="col-xl-4 col-lg-4 col-md-6">
-                    <div class="single_recipe text-center">
+                <div class="single_recipe text-center mb-20">
                         <div class="recipe_thumb">
                             <img src="img/recipe/rec1.jpg" alt="">
                         </div>
@@ -73,7 +84,7 @@
                     </div>
                 </div>
                 <div class="col-xl-4 col-lg-4 col-md-6">
-                    <div class="single_recipe text-center">
+                    <div class="single_recipe text-center mb-20">
                         <div class="recipe_thumb">
                             <img src="img/recipe/rec1.jpg" alt="">
                         </div>
@@ -84,7 +95,7 @@
                     </div>
                 </div>
                 <div class="col-xl-4 col-lg-4 col-md-6">
-                    <div class="single_recipe text-center">
+                    <div class="single_recipe text-center mb-20">
                         <div class="recipe_thumb">
                             <img src="img/recipe/rec1.jpg" alt="">
                         </div>
@@ -95,7 +106,7 @@
                     </div>
                 </div>
                 <div class="col-xl-4 col-lg-4 col-md-6">
-                    <div class="single_recipe text-center">
+                    <div class="single_recipe text-center mb-20">
                         <div class="recipe_thumb">
                             <img src="img/recipe/rec1.jpg" alt="">
                         </div>
@@ -106,18 +117,7 @@
                     </div>
                 </div>
                 <div class="col-xl-4 col-lg-4 col-md-6">
-                    <div class="single_recipe text-center">
-                        <div class="recipe_thumb">
-                            <img src="img/recipe/rec1.jpg" alt="">
-                        </div>
-                        <h3>Chowmein</h3>
-                        
-                        <p>Time Needs: 30 Mins</p>
-                        <button type="button" class="btn btn-outline-secondary">View Full Recipe</button>
-                    </div>
-                </div>
-                <div class="col-xl-4 col-lg-4 col-md-6">
-                    <div class="single_recipe text-center">
+                <div class="single_recipe text-center mb-20">
                         <div class="recipe_thumb">
                             <img src="img/recipe/rec1.jpg" alt="">
                         </div>

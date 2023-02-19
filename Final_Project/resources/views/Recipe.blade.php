d057c63658e8835c9966f502b675ed0850431ec6
karkikusal <karkikusal16@gmail.com>
2023-02-19 10:59:56 +0545

    update

--- Final_Project/resources/views/Recipe.blade.php
+++ Final_Project/resources/views/Recipe.blade.php
@@ -1,31 +1,22 @@
 @extends('layout.main')
 @section('content')
     
-<div class="bradcam_area  overlay" style="background-image : url('{{asset('img/recipe/recipe1.jpg')}}');">
-    <h3>RECIPE</h3>
+
+
+<div class="bradcam_area  overlay" style="background-image : url('{{asset('img/recipe/img1.jpg')}}');">
+    
 </div>
 
 
 
                           
 
-   <!--<div class="bradcam_area bradcam_bg_1">
-        <div class="container">
-            <div class="row">
-                <div class="col-xl-12">
-                    <div class="bradcam_text text-center">
-                        <h3> Recipes</h3>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
--->
+   
 
     
 
     
-    <div class="recipe_area plus_padding">
+    <div class="recipe_area plus_padding mt-5">
         <div class="container">
             <div class="row">
             <div class="col-xl-4 col-lg-4 col-md-6">
@@ -75,9 +66,9 @@
                 <div class="col-xl-4 col-lg-4 col-md-6">
                 <div class="single_recipe text-center mb-20">
                         <div class="recipe_thumb">
-                            <img src="img/recipe/rec1.jpg" alt="">
+                            <img src="img/recipe/pizza1.jpg" alt="">
                         </div>
-                        <h3>Chowmein</h3>
+                        <h3>Pizza</h3>
                         
                         <p>Time Needs: 30 Mins</p>
                         <button type="button" class="btn btn-outline-secondary">View Full Recipe</button>
@@ -86,9 +77,9 @@
                 <div class="col-xl-4 col-lg-4 col-md-6">
                     <div class="single_recipe text-center mb-20">
                         <div class="recipe_thumb">
-                            <img src="img/recipe/rec1.jpg" alt="">
+                            <img src="img/recipe/bur.jpg" alt="">
                         </div>
-                        <h3>Chowmein</h3>
+                        <h3>Burger</h3>
                         
                         <p>Time Needs: 30 Mins</p>
                         <button type="button" class="btn btn-outline-secondary">View Full Recipe</button>
@@ -97,9 +88,9 @@
                 <div class="col-xl-4 col-lg-4 col-md-6">
                     <div class="single_recipe text-center mb-20">
                         <div class="recipe_thumb">
-                            <img src="img/recipe/rec1.jpg" alt="">
+                            <img src="img/recipe/egg.jpg" alt="">
                         </div>
-                        <h3>Chowmein</h3>
+                        <h3>Egg curry</h3>
                         
                         <p>Time Needs: 30 Mins</p>
                         <button type="button" class="btn btn-outline-secondary">View Full Recipe</button>
@@ -108,9 +99,9 @@
                 <div class="col-xl-4 col-lg-4 col-md-6">
                     <div class="single_recipe text-center mb-20">
                         <div class="recipe_thumb">
-                            <img src="img/recipe/rec1.jpg" alt="">
+                            <img src="img/recipe/ice.jpg" alt="">
                         </div>
-                        <h3>Chowmein</h3>
+                        <h3>Vanilla ice-cream</h3>
                         
                         <p>Time Needs: 30 Mins</p>
                         <button type="button" class="btn btn-outline-secondary">View Full Recipe</button>
@@ -119,9 +110,9 @@
                 <div class="col-xl-4 col-lg-4 col-md-6">
                 <div class="single_recipe text-center mb-20">
                         <div class="recipe_thumb">
-                            <img src="img/recipe/rec1.jpg" alt="">
+                            <img src="img/recipe/thukpa.jpg" alt="">
                         </div>
-                        <h3>Chowmein</h3>
+                        <h3> Vegan noodles</h3>
                         
                         <p>Time Needs: 30 Mins</p>
                         <button type="button" class="btn btn-outline-secondary">View Full Recipe</button>

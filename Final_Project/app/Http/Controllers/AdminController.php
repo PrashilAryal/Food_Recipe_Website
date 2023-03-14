5a1dd2aab51f33cd6e54c393e38b00f21c832866
PrashilAryal <prashilaryal13@gmail.com>
2023-03-14 23:39:33 +0545

    Admin login UI upgraded

--- Final_Project/app/Http/Controllers/AdminController.php
+++ Final_Project/app/Http/Controllers/AdminController.php
@@ -2,26 +2,29 @@
 
 namespace App\Http\Controllers;
 use App\Models\Admin;
-
-
-
+use App\Models\Chef;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
 {
      public function login_admin(Request $req)
     {
          $req->validate([
-            'admin_email' => 'required',
-            'admin_password' => 'required'
+            // 'admin_email' => 'required',
+            // 'admin_password' => 'required'
+            'chef_email' => 'required',
+            'chef_password' => 'required'
         ]);
 
-        $admin = Admin::where('admin_email','=',$req->admin_email)->first();
+        // $admin = Admin::where('admin_email','=',$req->admin_email)->first();
+        $admin = Chef::where('chef_email','=',$req->chef_email)->first();
         
 
         if($admin)
         {
-            if($req->admin_password==$admin->admin_password)
+            // if($req->chef_password==$admin->chef_password)
+            if(Hash::check($req->chef_password,$admin->chef_password))
             {
                 return redirect()->route('adminpanel');
             }
